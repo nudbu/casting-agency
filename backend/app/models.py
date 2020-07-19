bf81df68c79bd0028628b2a2f9da1ee1b1032cf6
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-19 22:03:48 +0200

    Fix app logic/file structure

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from enum import Enum
-from app import app
+from app import db
 
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 class Gender(Enum):
     female = 1
@@ -17,16 +12,16 @@ class Actor(db.Model):
   name = db.Column(db.String(), nullable=False)
   age = db.Column(db.Integer, nullable=False)
   gender = db.Column(db.Enum(Gender), nullable=False)
-  movies = db.relationship('Movie_Cast', backref='ac')
+  movies = db.relationship('MovieCast', backref='ac')
 
 class Movie(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(), nullable=False)
   release_date = db.Column(db.Date, nullable=False)
-  cast = db.relationship('Movie_Cast', backref='mov')
+  cast = db.relationship('MovieCast', backref='mov')
 
 
-## Junction Table for many to many relationship between Actors and Movies
+## Junction Table for many-to-many relationship between Actors and Movies
 class MovieCast(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'), nullable=False)
