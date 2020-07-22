467057cad74ac5f6c4e50571f43cdb81f6f91055
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-22 15:03:39 +0200

    Refactor Use Application Factory Pattern

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,18 +3,24 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-from app import routes, models
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
+
+from app import models
--- backend/app/errors/handlers.py
+++ backend/app/errors/handlers.py
@@ -1,28 +1,28 @@
 from flask import jsonify
-from app import app
+from app.errors import bp
 
-@app.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found(error):
     return jsonify({
         'error message': 'Resource Not Found'
     }), 404
 
 
-@app.errorhandler(403)
+@bp.app_errorhandler(403)
 def forbidden(error):
     return jsonify({
         'error message': "No permission for requested action"
     }), 403
 
 
-@app.errorhandler(401)
+@bp.app_errorhandler(401)
 def forbidden(error):
     return jsonify({
         'error message': "Unauthenticated. Identity not verified"
     }), 401
 
 
-@app.errorhandler(500)
+@bp.app_errorhandler(500)
 def forbidden(error):
     return jsonify({
         'error message': "Internal Server Error"
