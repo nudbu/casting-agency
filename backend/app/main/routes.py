12539f8f7f12d6089a23b6e2d0fef73ce5969d97
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-25 18:17:47 +0200

    Add: Endpoint for Booking

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -2,10 +2,9 @@ from flask import jsonify, abort, request
 import datetime
 
 from app.main import bp
-from app.models import db, Actor, Movie
+from app.models import db, Actor, Movie, MovieCast
 
 #### Actor Endpoints
-
 
 ## Add actor to db
 @bp.route('/actors', methods=['POST'])
@@ -23,20 +22,13 @@ def add_actor():
     actor = Actor(name=name, age=age, gender=gender)
     db.session.add(actor)
     db.session.commit()
+    actor_id = actor.id
+
     return jsonify({
         'success': True,
-        'added actor': name
+        'added actor': name,
+        'actor_id': actor_id
     })
-
-
-## Get all actors
-@bp.route('/actors')
-def get_actors():
-    actors = Actor.query.all()
-    return jsonify({
-        'test': 'test'
-    })
-
 
 #### Movie Endpoints
 
@@ -45,15 +37,47 @@ def get_actors():
 def add_movie():
 
     #access request data
-    body = request.get_json()
-    title = body.get('title', None)
-    release_date_string = body.get('release_date')
-    release_date = datetime.datetime.strptime(release_date_string, '%Y-%m-%d')
+    try:
+        body = request.get_json()
+        title = body.get('title', None)
+        release_date_string = body.get('release_date')
+        release_date = datetime.datetime.strptime(release_date_string, '%Y-%m-%d')
+    except:
+        abort(422)
 
     movie = Movie(title=title, release_date=release_date)
     db.session.add(movie)
     db.session.commit()
+    movie_id = movie.id
+
     return jsonify({
         'success': True,
-        'added movie': title
+        'added movie': title,
+        'movie_id': movie_id
+    })
+
+
+#### Booking Endpoints
+
+## book Actor for Movie
+
+@bp.route('/contracts', methods=['POST'])
+def add_contract():
+    #access request data
+    try:
+        body = request.get_json()
+        movie_id = body.get('movie_id', None)
+        actor_id = body.get('actor_id', None)
+    except:
+        abort(422)
+
+    contract = MovieCast(actor_id=actor_id, movie_id=movie_id)
+    db.session.add(contract)
+    db.session.commit()
+
+    return jsonify({
+        'success': True,
+        'added contract': 'id',
+        'movie_id': movie_id,
+        'actor_id': actor_id 
     })
