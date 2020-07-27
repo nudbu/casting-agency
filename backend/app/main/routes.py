fca87b822b42bf13a2ee1ffd16aeb6faa3ca00fa
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-27 14:27:11 +0200

    Add: Get all actors/movies endpoint with pagination

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -4,6 +4,19 @@ import datetime
 from app.main import bp
 from app.models import db, Actor, Movie, MovieCast
 
+
+
+PAGINATE_LIMIT_DEFAULT = 15
+
+def paginate(request, selection):
+  offset = request.args.get('offset', 1, type=int)
+  limit = request.args.get('limit', PAGINATE_LIMIT_DEFAULT, type=int)
+  start_index = (offset - 1) * limit
+  end_index = start_index + limit
+
+  formatted_elements = [element.format() for element in selection[start_index:end_index]]
+  return formatted_elements
+
 #### Actor Endpoints
 
 ## Add actor to db
@@ -30,6 +43,18 @@ def add_actor():
         'actor_id': actor_id
     })
 
+## Get Actors paginated
+@bp.route('/actors')
+def get_all_actors():
+    actors = Actor.query.order_by(Actor.name).all()
+    formatted_actors_page = paginate(request, actors)
+
+    return jsonify({
+        'success': True,
+        'actors': formatted_actors_page,
+        'total_actors': len(actors)
+    })
+
 #### Movie Endpoints
 
 ## Add movie to db
@@ -54,6 +79,18 @@ def add_movie():
         'success': True,
         'added movie': title,
         'movie_id': movie_id
+    })
+
+## Get Movies paginated
+@bp.route('/movies')
+def get_all_movies():
+    movies = Movie.query.order_by(Movie.title).all()
+    formatted_movies_page = paginate(request, movies)
+
+    return jsonify({
+        'success': True,
+        'actors': formatted_movies_page,
+        'total_movies': len(movies)
     })
 
 
