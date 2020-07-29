5e6245cbace1c933fd8e8366dd68bcdda607473b
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-29 13:48:48 +0200

    Change: Add massage in errorhandlers

--- backend/app/errors/handlers.py
+++ backend/app/errors/handlers.py
@@ -4,6 +4,7 @@ from app.errors import bp
 @bp.app_errorhandler(404)
 def not_found(error):
     return jsonify({
+        'success': False,
         'error message': 'Resource Not Found'
     }), 404
 
@@ -11,6 +12,7 @@ def not_found(error):
 @bp.app_errorhandler(403)
 def forbidden(error):
     return jsonify({
+        'success': False,
         'error message': "No permission for requested action"
     }), 403
 
@@ -18,18 +20,21 @@ def forbidden(error):
 @bp.app_errorhandler(401)
 def forbidden(error):
     return jsonify({
+        'success': False,
         'error message': "Unauthenticated. Identity not verified"
     }), 401
 
 @bp.app_errorhandler(422)
 def unprocessable(error):
     return jsonify({
+        'success': False,
         'error message': "Unprocessable Entity"
     }), 422
 
 @bp.app_errorhandler(500)
 def forbidden(error):
     return jsonify({
+        'success': False,
         'error message': "Internal Server Error"
     }), 500
     
