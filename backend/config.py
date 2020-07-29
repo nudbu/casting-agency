cec46516508a76c5c29303defe79ed433dfc1b89
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-29 22:19:59 +0200

    Change: look for database_uri in environment first, then hardcode

--- backend/config.py
+++ backend/config.py
@@ -10,4 +10,4 @@ database_path = f"postgresql://{postgres_user}@{host}:{port}/{database_name}"
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    SQLALCHEMY_DATABASE_URI = database_path
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or database_path
