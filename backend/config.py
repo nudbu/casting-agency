6b1a267d36ad90c09424e18c70308ccaeb3907a4
Juliane Ratzsch <ratzsch.j@gmail.com>
2020-07-31 00:23:45 +0200

    Take Tokens from environment

--- backend/config.py
+++ backend/config.py
@@ -8,6 +8,6 @@ port = "5432"
 database_path = f"postgresql://{postgres_user}@{host}:{port}/{database_name}"
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-me'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or database_path
